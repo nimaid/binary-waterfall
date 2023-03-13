7f0e726ab2c513ca51866946e56478237be73c90
Ella Jameson <ellagjameson@gmail.com>
2023-03-13 00:01:58 -0700

    Object oriented

--- binary-waterfall.py
+++ binary-waterfall.py
@@ -1,5 +1,6 @@
 import os
 import wave
+from PIL import Image
 import pygame
 
 # https://www.youtube.com/watch?v=NFe0aGO9-TE
@@ -13,6 +14,9 @@ class BinaryWaterfall:
             self.bytes = f.read()
         
         self.wav_filename = None
+        self.wav_channels = None
+        self.wav_sample_bytes = None
+        self.wav_sample_rate = None
     
     def get_hex(self, caps=True):
         file_hex = self.bytes.hex()
@@ -22,19 +26,23 @@ class BinaryWaterfall:
     
     def save_audio_file(
         self,
-        filename=None,
-        channels=1,
-        sample_bytes=1,
-        framerate=32000
+        filename=None, # Will set itself based on the input filename if left set to None
+        channels=1, # Number of channels, 1 = Mono, 2 = Stereo, etc.
+        sample_bytes=1, # Number of bytes per sample, 1 = 8-bit, 2 = 16-bit, etc.
+        sample_rate=32000 # The sample rate of the file
     ):
         if filename is None:
             filename = self.filename + os.path.extsep + "wav"
         
         with wave.open(filename, "wb") as f:
             f.setnchannels(channels)
-            f.setsampwidth(sample_bytes) # Number of bytes, 1 = 8-bit, 2 = 16-bit, etc.
-            f.setframerate(framerate) # Also called sample rate
+            f.setsampwidth(sample_bytes)
+            f.setframerate(sample_rate)
             f.writeframesraw(self.bytes)
+        
+        self.wav_channels = channels
+        self.wav_sample_bytes = sample_bytes
+        self.wav_samplerate = sample_rate
         
         self.wav_filename = filename
         return self.wav_filename
