5fda158721579f0facc7037c46c2e698a9464a85
Ella Jameson <ellagjameson@gmail.com>
2023-03-13 01:23:48 -0700

    Visuals recreated

--- binary-waterfall.py
+++ binary-waterfall.py
@@ -1,14 +1,21 @@
 import os
 import wave
-from PIL import Image
-import pygame
+import contextlib
+with contextlib.redirect_stdout(None):
+    import pygame
 
 # https://www.youtube.com/watch?v=NFe0aGO9-TE
 # https://www.youtube.com/watch?v=HFgqyB7hm3Y
 
 class BinaryWaterfall:
-    def __init__(self, filename):
+    def __init__(self, filename, width=48, height=48):
         self.filename = filename
+        
+        self.width = width
+        self.height = height
+        self.used_color_bytes = 3
+        self.dead_color_bytes = 1
+        self.color_bytes = self.used_color_bytes + self.dead_color_bytes
         
         with open(self.filename, "rb") as f:
             self.bytes = f.read()
@@ -49,20 +56,61 @@ class BinaryWaterfall:
     
     def get_image(
         self,
-        address,
-        width=24,
-        height=24
+        address
     ):
         picture_bytes = bytes()
         current_address = address
-        for row in range(height):
-            for col in range(width):
+        for row in range(self.height):
+            for col in range(self.width):
                 picture_bytes += self.bytes[current_address:current_address+1] # Red
                 current_address += 1
                 picture_bytes += self.bytes[current_address:current_address+1] # Green
                 current_address += 1
                 picture_bytes += self.bytes[current_address:current_address+1] # Blue
                 current_address += 1
+                
+                current_address += self.dead_color_bytes # Skip bytes for visual
         
-        picture = Image.frombytes('RGB', (width, height), picture_bytes)
+        full_length = (self.width * self.height * self.used_color_bytes)
+        picture_bytes_length = len(picture_bytes)
+        # Pad picture data
+        if picture_bytes_length < full_length:
+            pad_length = full_length - picture_bytes_length
+            picture_bytes += b"\x00" * pad_length
+        # Tell that we are past the end of the file
+        if picture_bytes_length == 0:
+            current_address = -1
+        picture = pygame.image.frombytes(picture_bytes, (self.width, self.height), 'RGB')
         return picture, current_address
+
+
+
+pygame.init()
+X = 600
+Y = 600
+
+screen = pygame.display.set_mode((X, Y))
+pygame.display.set_caption('image')
+
+waterfall = BinaryWaterfall(os.path.sep.join(["examples", "mspaint.exe"]))
+waterfall.save_audio_file() # Create wave file
+
+run_program = True
+address = 0
+while run_program:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            run_program = False
+    
+    image, end_address = waterfall.get_image(address)
+    image = pygame.transform.scale(image, (X, Y))
+    screen.blit(image, (0, 0))
+    pygame.display.flip()
+    
+    address += waterfall.width * waterfall.color_bytes
+    
+    if end_address == -1:
+        run_program = False
+    
+    pygame.time.wait(10)
+pygame.quit()
