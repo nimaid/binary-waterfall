e2ee20c4bf920e671353900b8a125c7a342fb366
Ella Jameson <ellagjameson@gmail.com>
2023-03-12 22:31:01 -0700

    Made wave file

--- binary-waterfall.py
+++ binary-waterfall.py
@@ -1,14 +1,18 @@
 import os
+import wave
 
 # https://www.youtube.com/watch?v=NFe0aGO9-TE
 # https://www.youtube.com/watch?v=HFgqyB7hm3Y
-# https://stackoverflow.com/questions/52369925/creating-wav-file-from-bytes
+
 class BinaryWaterfall:
     def __init__(self, filename):
-        self.name = filename
+        self.filename = filename
         
-        with open(self.name, "rb") as f:
+        with open(self.filename, "rb") as f:
             self.bytes = f.read()
+        
+        self.hex_filename = None
+        self.wav_filename = None
     
     def get_hex(self, caps=True):
         file_hex = self.bytes.hex()
@@ -16,14 +20,37 @@ class BinaryWaterfall:
             file_hex = file_hex.upper()
         return file_hex
     
-    def save_hex(
+    def save_hex_file(
         self,
         filename=None,
         caps=True
     ):
         if filename is None:
-            filename = self.name + os.path.extsep + "txt"
+            filename = self.filename + os.path.extsep + "txt"
+        
         file_hex = self.get_hex(caps=caps)
         with open(filename, "w") as f:
             f.write(file_hex)
-        return filename
+        
+        self.hex_filename = filename
+        return self.hex_filename
+    
+    def save_audio_file(
+        self,
+        filename=None,
+        channels=1,
+        sample_bytes=1,
+        framerate=32000
+    ):
+        if filename is None:
+            filename = self.filename + os.path.extsep + "wav"
+        
+        with wave.open(filename, "wb") as f:
+            f.setnchannels(channels)
+            f.setsampwidth(sample_bytes) # Number of bytes, 1 = 8-bit, 2 = 16-bit, etc.
+            f.setframerate(framerate) # Also called sample rate
+            f.writeframesraw(self.bytes)
+        
+        self.wav_filename = filename
+        return self.wav_filename
+        
