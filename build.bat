39e243cb5ab51312e528d3333eb470583f346ca2
Ella Jameson <ellagjameson@gmail.com>
2023-11-05 17:24:16 -0700

    Added version info

--- build.bat
+++ build.bat
@@ -1,19 +1,29 @@
 @echo off
 
+set MAINFAILENAME=binary-waterfall
+set ENVNAME=%MAINFAILENAME%
+
 set ORIGDIR=%CD%
 set DISTDIR=%ORIGDIR%\dist
 set BUILDDIR=%ORIGDIR%\build
 
-set PY=%ORIGDIR%\binary-waterfall.py
-set SPEC=%ORIGDIR%\binary-waterfall.spec
-set EXE=%DISTDIR%\binary-waterfall.exe
+set PY=%ORIGDIR%\%MAINFAILENAME%.py
+set SPEC=%ORIGDIR%\%MAINFAILENAME%.spec
+set EXE=%DISTDIR%\%MAINFAILENAME%.exe
+
+set VERSION_INFO=%ORIGDIR%\file_version_info.txt
+
 
 echo Building portable EXE...
-call conda run -n binary-waterfall pyinstaller ^
+call conda run -n %ENVNAME% create-version-file version.yml --outfile %VERSION_INFO%
+if errorlevel 1 goto ERROR
+call conda run -n %ENVNAME% pyinstaller ^
+    --clean ^
     --noconfirm ^
 	--add-data icon.png;. ^
     --onefile ^
     --icon=icon.ico ^
+    --version-file=%VERSION_INFO% ^
     "%PY%"
 if errorlevel 1 goto ERROR
 
@@ -24,6 +34,7 @@ rmdir /s /q "%DISTDIR%" 1>nul 2>&1
 del /f /s /q "%BUILDDIR%" 1>nul 2>&1
 rmdir /s /q "%BUILDDIR%" 1>nul 2>&1
 del /f /q "%SPEC%" 1>nul 2>&1
+del /f /q "%VERSION_INFO%" 1>nul 2>&1
 
 goto DONE
 
